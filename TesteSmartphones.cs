6ed77485ffe1da916aca1c92580366840311ba25
João Dantas <joaogdantasdev@hotmail.com>
2023-12-31 16:56:01 -0300

    feat: criadas instâncias das classes Nokia e IPhone para testes

---
+++ TesteSmartphones.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "84900000000", modelo: "Tijolão", imei: "000000000", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "8490000000", modelo: "12", imei: "111111111", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
