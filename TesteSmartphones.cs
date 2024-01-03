06322e96d33518dae539b74aa94ec6f617ab4c33
João Dantas <joaogdantasdev@hotmail.com>
2024-01-02 21:07:51 -0300

    feat: adicionados testes para quando os parâmetros número e memória são passados de forma errada

--- TesteSmartphones.cs
+++ TesteSmartphones.cs
@@ -1,13 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-Console.WriteLine("Smartphone Nokia: ");
+Console.WriteLine("Smartphone Nokia (Parâmetros Corretos): ");
 Smartphone nokia = new Nokia(numero: "84900000000", modelo: "Tijolão", imei: "000000000", memoria: 64);
 nokia.Ligar();
 nokia.ReceberLigacao();
 nokia.InstalarAplicativo("WhatsApp");
 
-Console.WriteLine("\nSmartphone Iphone: ");
+Console.WriteLine("\nSmartphone Iphone (Parâmetros Corretos): ");
 Smartphone iphone = new Iphone(numero: "84900000000", modelo: "12", imei: "111111111", memoria: 128);
-nokia.Ligar();
-nokia.ReceberLigacao();
-nokia.InstalarAplicativo("Instagram");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\nSmartphone Iphone (Memória com valor não permitido): ");
+Smartphone iphoneMemoriaInvalida = new Iphone(numero: "84900000000", modelo: "12", imei: "111111111", memoria: 200);
+
+Console.WriteLine("\nSmartphone Iphone (Número Inválido): ");
+Smartphone iphoneNumeroInvalido = new Iphone(numero: "8490000000000", modelo: "12", imei: "111111111", memoria: 128);
